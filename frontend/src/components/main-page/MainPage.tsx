baec2ad8b22eb5810e9fe308995e916694d5ab17
Paul Gillis <paul.gillis@quarkworks.co>
2023-08-22 13:48:35 -0700

    Trying to get a navbar going

--- frontend/src/components/main-page/MainPage.tsx
+++ frontend/src/components/main-page/MainPage.tsx
@@ -1,17 +1,13 @@
+import { Link } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
+import Navbar from '../navbar/navbar';
 
 function MainPage() {
     const { logout } = useAuth();
 
     return (
         <>
-        <p>whoops</p>
-        <button
-            onClick={logout}
-            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-            Logout
-        </button>
+        <Navbar />
         </>
     )
 }
