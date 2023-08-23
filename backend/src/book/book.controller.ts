721e8c1ce269d3170215c9adb1963b20f69589be
Paul Gillis <pauldg-github@postmail.ovh>
2023-08-23 13:44:43 -0700

    Start work on file upload

--- backend/src/book/book.controller.ts
+++ backend/src/book/book.controller.ts
@@ -1,69 +1,51 @@
 import {
   Controller,
-  FileValidator,
   HttpStatus,
-  Injectable,
   ParseFilePipeBuilder,
   Post,
   UploadedFile,
-  // UseGuards,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
-import { IFile } from '@nestjs/common/pipes/file/interfaces';
 import { FileInterceptor } from '@nestjs/platform-express';
-// import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { BookService } from './book.service';
 import { diskStorage } from 'multer';
-import { extname } from 'path';
-
-@Injectable()
-export class BookValidor extends FileValidator {
-  isValid<TFile extends IFile>(file?: TFile): boolean | Promise<boolean> {
-    return (
-      file.mimetype === 'application/octet-stream' &&
-      file['originalname'] &&
-      file['originalname'].split(/[. ]+/).pop() ===
-        this.validationOptions['fileext']
-    );
-  }
-  buildErrorMessage(): string {
-    const ext = this.validationOptions['fileext'];
-    return 'Not an ' + ext + ' file.';
-  }
-}
+import { extname, join as pathJoin } from 'path';
+import { existsSync as fsExistsSync, mkdirSync as fsMkdirSync } from 'fs';
 
 @Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
-  // @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @UseInterceptors(
     FileInterceptor('file', {
       storage: diskStorage({
-        destination: './uploads',
-        filename: (req, file, cb) => {
-          console.log(req.user);
-          const randomName = Array(32)
-            .fill(null)
-            .map(() => Math.round(Math.random() * 16).toString(16))
-            .join('');
-          //Calling the callback passing the random name generated with the original extension name
-          cb(null, `${randomName}${extname(file.originalname)}`);
+        destination: (req, _file, cb) => {
+          const userName = req.user['username'];
+          const destPath = pathJoin('uploads', userName);
+          if (!fsExistsSync(destPath)) {
+            fsMkdirSync(destPath);
+          }
+          cb(null, destPath);
+        },
+        filename: (_req, file, cb) => {
+          const newFileName = Date.now() + file.originalname;
+          cb(null, newFileName);
         },
       }),
+      fileFilter: (_req, file, callback) => {
+        callback(null, extname(file.originalname) === '.epub');
+      },
     }),
   )
   @Post('upload')
   async uploadBook(
     @UploadedFile(
       new ParseFilePipeBuilder()
-        .addValidator(new BookValidor({ fileext: 'epub' }))
-        .addMaxSizeValidator({
-          maxSize: 20000000,
-        })
-        .build({
-          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
-        }),
+        .addMaxSizeValidator({ maxSize: 20000000 })
+        .build({ errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY }),
     )
     file: Express.Multer.File,
   ) {
--- backend/src/book/book.service.ts
+++ backend/src/book/book.service.ts
@@ -1,15 +1,52 @@
 import { Injectable } from '@nestjs/common';
+import { readFileSync as fsReadFileSync } from 'fs';
+import {
+  configure,
+  ZipReader,
+  BlobReader,
+  TextWriter,
+  BlobWriter,
+} from '@zip.js/zip.js';
+import { EPUB } from 'foliate-js/EPUB';
 
-function toArrayBuffer(buffer) {
-  return buffer.buffer.slice(
-    buffer.byteOffset,
-    buffer.byteOffset + buffer.byteLength,
+// function toArrayBuffer(buffer: Buffer) {
+//   return buffer.buffer.slice(
+//     buffer.byteOffset,
+//     buffer.byteOffset + buffer.byteLength,
+//   );
+// }
+
+const makeZipLoader = async (file) => {
+  configure({ useWebWorkers: false });
+  const reader = new ZipReader(new BlobReader(file));
+  const entries = await reader.getEntries();
+  const map = new Map(entries.map((entry) => [entry.filename, entry]));
+  const load =
+    (f) =>
+    (name, ...args) =>
+      map.has(name) ? f(map.get(name), ...args) : null;
+  const loadText = load((entry) => entry.getData(new TextWriter()));
+  const loadBlob = load((entry, type) => entry.getData(new BlobWriter(type)));
+  const getSize = (name) => map.get(name)?.uncompressedSize ?? 0;
+  return { entries, loadText, loadBlob, getSize };
+};
+
+const isZip = async (file: Buffer) => {
+  const arr = new Uint8Array(await file.slice(0, 4));
+  return (
+    arr[0] === 0x50 && arr[1] === 0x4b && arr[2] === 0x03 && arr[3] === 0x04
   );
-}
+};
 
 @Injectable()
 export class BookService {
   async saveBook(file: Express.Multer.File) {
+    const buff = fsReadFileSync(file.path, null);
+    const yoZip = await isZip(buff);
+    console.log(yoZip);
+    const zipLoader = makeZipLoader(buff);
+    const book = await new EPUB(zipLoader).init();
+    console.log(book);
     // const arrayBuff = toArrayBuffer(file.buffer);
     // const book = ePub(arrayBuff);
     // await Promise.all([book.loaded.metadata, book.coverUrl()]).then(
